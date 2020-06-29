ac433bd11039ed4a358d76b5ded2504ea08454dc
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-29 16:59:53 +0300

    Notes added

---
+++ My Work/app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: "#app",
+    data: {
+        inComingData: "Hello Vue!",
+        number: 0,
+        number1: 5,
+        number2: 4,
+        result: '',
+    },
+    methods: {
+
+        sayiArttir() {
+            this.number += 1;
+        },
+
+        sayilarToplami(){
+           this.result = this.number1 + this.number2;
+        }
+    }
+
+})
