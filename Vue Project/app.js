fedc97d33cefaa34b72a99fc815272e4feb7aea2
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-01 00:05:45 +0300

    Project added

--- Vue Project/app.js
+++ Vue Project/app.js
@@ -1,29 +1,46 @@
 new Vue({
     el: '#app',
     data: {
-        firstNumber: '',
-        secondNumber: '',
-        result: '',
-        collections: [
-        
-        ],
-
+      firstNumber: '',
+      secondNumber: '',
+      result: '',
+      collections: [],
     },
     methods: {
-        collection: function () {
-            return this.result = this.firstNumber + this.secondNumber;
-        },
 
-        addResult: function () {
-            this.collections.push(this.result);
-        },
+      signalChange: function (evt) {
+        this.$emit((evt.srcElement.innerText = ''));
+      },
+
+      collection: function (event) {
+        const element = event.target;
+        element.disabled = true;
+        setTimeout(() => {
+          this.result = 0;
+          this.firstNumber = 0;
+          this.secondNumber = 0;
+        }, 500);
+        element.disabled = false;
+        this.result = this.firstNumber + this.secondNumber;
+      },
+  
+      addResult: function () {
+        this.collections.push(`${this.firstNumber} + ${this.secondNumber} = ${this.result}`);
+      },
     },
+
     computed: {
-        collectionResult() {
-            return "Sonuç: ";
-        },
+      collectionResult() {
+        return 'Sonuç: ';
+      },
     },
+
     watch: {
+      value(value) {
+        this.$emit('onchange', value);
+        // or generate/simulate a native events (not sure how, but its outside Vue's realm I think
+      },
 
     }
-})
+    
+  });
