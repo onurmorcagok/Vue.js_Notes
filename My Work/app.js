a951f98c76132bc9e14fcbd2f43ab32e682a8057
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-03 16:17:36 +0300

    Notes added

--- My Work/app.js
+++ My Work/app.js
@@ -1,7 +1,17 @@
 new Vue({
     el: "#app",
     data: {
-
+        message: 'Hello Vue.js',
+        anotherMessage: 'Vue incredible framework',
+        userComment: '',
+        userComments: [
+            
+        ],
     },
-    
+    methods:{
+        addUserComment:function(){
+            this.userComments.push(this.userComment);
+        }
+    }
+
 })
