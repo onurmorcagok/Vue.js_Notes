5886fd56ecdfe31fc50682aa9b47c2e578450d11
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-08 14:02:30 +0300

    Repo updated

---
+++ Vue Projects/Project 1/app.js
@@ -0,0 +1,42 @@
+new Vue({
+
+    el: '#app',
+    data: {
+
+        firstNumber: '',
+        secondNumber: '',
+        result: '',
+        isShow: true,
+        clicked:true,
+        collections: [
+
+        ],
+    },
+
+    methods: {
+
+        collection: function (event) {
+            const element = event.target;
+            element.disabled = true;
+            setTimeout(() => {
+                this.result = '';
+                this.firstNumber = '';
+                this.secondNumber = '';
+            }, 500);
+            element.disabled = false;
+            this.result = this.firstNumber + this.secondNumber;
+        },
+
+        addResult: function () {
+            this.collections.push(`${this.firstNumber} + ${this.secondNumber} = ${this.result}`);
+        },
+    },
+
+    computed: {
+
+        collectionResult() {
+            return 'Sonuç: ';
+        },
+    },
+
+});
