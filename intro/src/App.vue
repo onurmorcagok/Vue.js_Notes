0d845d2ea5c02914ae126e8877899d413b429852
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-10 01:22:38 +0300

    Notes added

--- intro/src/App.vue
+++ intro/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    Hello {{ name }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      name: "Onur",
+      number1: 21,
+      number2: 12,
+      result: null
+    };
+  },
+  methods: {
+    topla() {
+      this.result = this.number1 + this.number2;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
