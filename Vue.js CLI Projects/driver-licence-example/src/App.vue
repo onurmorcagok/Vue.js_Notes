cdd280a87859a06a6557e290ab55839bee0d9a5f
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-11 01:36:44 +0300

    Notes added

--- Vue.js CLI Projects/driver-licence-example/src/App.vue
+++ Vue.js CLI Projects/driver-licence-example/src/App.vue
@@ -1,14 +1,85 @@
 <template>
-  <div>
-      
+
+  <div class="mt-5 px-1">
+    <div class="row">
+      <div class="col-md-6">
+        <ul>
+          <li v-for="user in users" v-bind:key="user.id">
+            <a @click="selectedUser = user" 
+            class="text-info">{{ `${user.name} ${user.surname}` }}
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-6">
+        <IdentityComponent v-if="selectedUser != null"
+                           :name="selectedUser.name"
+                           :surname="selectedUser.surname"
+                           :driverLicenceObject="selectedUser.driverLicence" 
+        />
+      </div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
+import IdentityComponent from "./components/IdentityComponent";
 
 export default {
-  name: 'App',
-  
-}
+  name: "App",
+  components: {
+    IdentityComponent,
+  },
+  data() {
+    return {
+      users: [
+        {
+          name: "Jennifer",
+          surname: "Lopez",
+          driverLicence: {
+            photo:
+              "https://randomuser.me/api/portraits/women/67.jpg",
+            licenceID: "ABC1234",
+            licenceType: "B,A/2",
+            publishDate: "02.06.1995",
+            expireDate: "02.06.2005"
+          },
+          address: {
+            city: "",
+            postCode: ""
+          }
+        },
+        {
+          name: "Michael",
+          surname: "Jordan",
+          driverLicence: {
+            photo:
+              "https://randomuser.me/api/portraits/men/45.jpg",
+            licenceID: "ABC2345",
+            licenceType: "B",
+            publishDate: "13.09.1998",
+            expireDate: "13.09.2008"
+          }
+        },
+        {
+          name: "David",
+          surname: "Beckham",
+          driverLicence: {
+            photo:
+              "https://randomuser.me/api/portraits/men/62.jpg",
+            licenceID: "ABC5896",
+            licenceType: "B,A/2",
+            publishDate: "24.06.1999",
+            expireDate: "24.06.2009"
+          }
+        }
+      ],
+      selectedUser: null
+    };
+  },
+};
 </script>
 
---
+++ Vue.js CLI Projects/driver-licence-example/src/components/IdentityComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card bg-danger">
+    <div class="row">
+      <div class="col-6">
+        <img class="w-50" :src="driverLicenceObject.photo" />
+      </div>
+
+      <div class="col-6 text-left text-white">
+        <div>Name Surname: {{name}} {{surname}}</div>
+        <div>Licence ID: {{driverLicenceObject.licenceID}}</div>
+        <div>Licence Type: {{driverLicenceObject.licenceType}}</div>
+        <div>Publish Date: {{driverLicenceObject.publishDate}}</div>
+        <div>Expire Date: {{driverLicenceObject.expireDate}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IdentityComponent",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    surname: {
+      type: String,
+      required: true
+    },
+    driverLicenceObject: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
