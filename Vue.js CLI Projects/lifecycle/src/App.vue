d3a6735a36fa82bba2731f56bc7ee4a6aa4073f1
OnurMorcagok <onurmorcagok@gmail.com>
2020-07-12 23:53:46 +0300

    Notes added

---
+++ Vue.js CLI Projects/lifecycle/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <firstComponent>
+      
+    </firstComponent>
+  </div>
+</template>
+
+<script>
+
+  import firstComponent from "./components/firstComponent";
+
+export default {
+  name: 'App',
+  components: {
+    firstComponent,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue.js CLI Projects/lifecycle/src/components/firstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      First Component
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'firstComponent',
+
+}
+</script>
+
+<style>
+
+</style>
