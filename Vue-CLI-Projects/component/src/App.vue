434445c85c6a065475924edb8b78c9367f8943b1
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-07-28 16:29:11 +0300

    Notes added

---
+++ Vue-CLI-Projects/component/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <lampComponent :key="lamp" v-for="lamp in 5"/>
+  </div>
+</template>
+
+<script>
+
+import lampComponent from './components/lampComponent';
+
+export default {
+
+  name: 'App',
+  components: {
+    lampComponent,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue-CLI-Projects/component/src/components/lampComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Lamba Durumu: {{ status }}</p>
+    <hr />
+    <button @click="openLamp">Lambayı Aç</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "lampComponent",
+  data() {
+    return {
+      status: "Kapalı",
+    };
+  },
+  methods: {
+    openLamp() {
+        this.status = "Açık";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
