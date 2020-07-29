698daafc6325c2bf3411455d3511dfc5abd8f474
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-07-30 01:30:35 +0300

    Notes added

---
+++ Vue-CLI-Projects/eventBus/event-bus/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <p>{{ copyright }}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main";
+
+export default {
+  name: "Footer",
+  data() {
+    return {
+      copyright: "Onur 2020",
+    };
+  },
+  created() {
+    eventBus.$on("changeTitle", (data) => {
+      this.copyright = data;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid black;
+  text-align: center;
+  width: auto;
+}
+</style>
---
+++ Vue-CLI-Projects/eventBus/event-bus/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card" >
+        <h1 @click="changeTitle">{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main'
+    export default {
+        name: 'Header',
+        data() {
+            return {
+                title: 'Onur M.',
+            }
+        },
+        methods:{
+            changeTitle(){
+                //this.$emit('changeTitle','Onur MORCAGÖK');
+                this.title = "Onur MORCAGÖK";
+                eventBus.$emit('changeTitle',this.title);
+            }
+        }
+    }
+</script>
+
+<style>
+    .card {
+        margin:20px;
+        padding:20px;
+        border:1px solid black;
+        text-align: center;
+        width: auto;
+    }
+</style>
