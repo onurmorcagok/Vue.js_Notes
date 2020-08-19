03262c0ec1c77555f99d1e06d7e575d95d33097a
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-08-19 17:43:15 +0300

    Project added

---
+++ Vue-Projects/imdb-movie-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <appNavbar></appNavbar>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    appNavbar: Navbar,
+  },
+  created(){
+    window.vueInstance = this;
+  }
+}
+</script>
---
+++ Vue-Projects/imdb-movie-project/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">IMDB Movie Project</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <router-link to="/" class="nav-link">Home</router-link>
+          <router-link to="/favorite" class="nav-link">Favorite</router-link>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
